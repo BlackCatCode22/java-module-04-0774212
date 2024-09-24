25d4b28b76b4c20ac33c1fdd794ea892405e3e64
0774212 <lrd98@yahoo.com>
2024-09-24 11:54:34 -0700

    Add files via upload

---
+++ ArrayListPractice.java
@@ -0,0 +1,57 @@
+// gtc 9/24/24
+// ArrayListPractice.java
+
+import java.util.ArrayList;
+
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+
+        System.out.printf("\n Welcome to the ArrayList Practice program.");
+
+        // Create an ArrayList of Strings
+        ArrayList<String> myListOfStrings = new ArrayList<>();
+
+        // Add a few elements to my ArrayList.
+        myListOfStrings.add("first");
+        myListOfStrings.add("second");
+        myListOfStrings.add("third");
+
+        // Output our ArrayList.
+        System.out.println("\n myListOfStrings is: " + myListOfStrings);
+
+        //Remove an element.
+        myListOfStrings.remove(0);
+
+        // Output our ArrayList.
+        System.out.println("\n After removing...");
+        System.out.println("\n myListOfStrings is: " + myListOfStrings);
+
+        //Remove an element.
+        myListOfStrings.remove("third");
+
+        // Output our ArrayList.
+        System.out.println("\n After removing...");
+        System.out.println("\n myListOfStrings is: " + myListOfStrings);
+
+        // use a for loop to add some things to our list.
+        String toAdd = "";
+
+
+        for (int i=0; i<100; i++) {
+            toAdd = toAdd + " adding..." + Integer.toString(i);
+            myListOfStrings.add(toAdd);
+        }
+
+        // Output our ArrayList.
+        System.out.println("\n After adding a bunch of stuff...");
+        System.out.println("\n myListOfStrings is: " + myListOfStrings);
+
+        // Use a for loop to output each element of our ArrayList
+        for (int i=0; i<myListOfStrings.size(); i++) {
+            System.out.println("\n An element in my list is: " + myListOfStrings.get(i));
+        }
+
+
+    }
+}
